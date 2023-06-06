4ab00520dfb9819247f58a7da4a5c84a8fff5fa4
AngiraDas <117512551+AngiraDas@users.noreply.github.com>
2023-06-07 05:17:15 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -16,16 +16,37 @@ class Solution
         }
         if(dp[i][w]!=-1) return dp[i][w];
         int not_pick=f(i-1,w,wt,val,n,dp);
-        int pick=0;
+        int pick=-1e9;
         if(wt[i]<=w) pick= val[i]+f(i-1,w-wt[i],wt,val,n,dp);
         return dp[i][w]=max(pick,not_pick);
     }
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        vector<vector<int>>dp(n,vector<int>(W+1,-1));
-        return f(n-1,W,wt,val,n,dp);
-       // Your code here
+        
+         //memoization
+        // vector<vector<int>>dp(n,vector<int>(W+1,-1));
+        // return f(n-1,W,wt,val,n,dp);
+        
+        
+        
+        
+       //tabulation
+        vector<vector<int>>dp(n,vector<int>(W+1,0));
+       for(int i=wt[0];i<=W;i++){
+           dp[0][i] =val[0];
+          //every weight greater than the wt of the first element of wt array can make us take the value at index 0 .
+       }
+       
+       for(int i=1;i<n;i++){
+           for(int j=0;j<=W;j++){
+               int not_pick=dp[i-1][j];
+               int pick=-1e9;
+               if(wt[i]<=j) pick=val[i]+dp[i-1][j-wt[i]];
+               dp[i][j]=max(pick,not_pick);
+           }
+       }
+       return dp[n-1][W];
     }
 };
 
